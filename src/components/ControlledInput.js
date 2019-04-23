ccb67901f95df89f6da57a5fced2f18cbe05f2d1
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-04-23 17:26:49 -0400

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,38 @@
 // Code ControlledInput Component Here
+import React, { Component } from 'react'
+
+export default class ControlledInput extends Component {
+
+  state = {
+    firstName: 'Khaled',
+    lastName: 'Khalifeh'
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render() {
+    const { firstName, lastName } = this.state
+
+
+    return (
+      <form>
+        <input
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={this.onChange}
+        />
+        <input
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={this.onChange}
+        />
+      </form>
+    )
+  }
+}
